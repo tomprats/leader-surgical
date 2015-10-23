810cd3f2958ee384d92fe04c88b6d7f52a95e1e9
Tom Prats <tprats108@gmail.com>
2015-10-22 23:52:53 -0400

    Doctors, History, Beautify, Reorganization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,10 @@ module ApplicationHelper
   def navbar_links
     [
       { text: "Our Vision", path: vision_path },
+      { text: "Doctors",    path: doctors_path },
       { text: "Services",   path: services_path },
       { text: "Your Visit", path: visit_path },
-      { text: "FAQ",        path: faq_path },
-      { text: "Feedback",   path: feedback_path }
+      { text: "FAQ",        path: faq_path }
     ]
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 
   controller :pages do
     get :vision
+    get :doctors
     get :services
     get :visit
     get :faq
-    get :feedback
   end
 end
