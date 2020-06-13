86b75f50bc41489e7324e0f6ac020aa59834cd70
Tom Prats <tprats108@gmail.com>
2020-06-12 22:59:20 -0400

    Added new payment link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,6 @@ module ApplicationHelper
       { text: "Our Vision", path: vision_path },
       { text: "Doctors",    path: doctors_path },
       { text: "Services",   path: services_path },
-      { text: "Your Visit", path: visit_path },
       { text: "FAQ",        path: faq_path }
     ]
   end
